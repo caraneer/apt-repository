f21c0f159d3b22103d7a9092398adf56180c0283
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2025-04-25 16:05:37 -0400

    complete initial version of nginx and acmetool config

---
+++ configurators/nginx/mkdeb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+SOURCEDIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P);
+WORKDIR="$(mktemp -d -t nginx-deb.XXXXXXXXXX)"
+echo "WORKDIR=$WORKDIR";
+PKGROOT="$WORKDIR/pkg"
+version="0.1.0-1"
+
+echo "Preparing package directory..."
+mkdir -p "$PKGROOT";
+
+# Copy package files
+cp -r "$SOURCEDIR/DEBIAN" "$PKGROOT/DEBIAN"
+mkdir -p "$PKGROOT/usr/share/caraneer-config-nginx";
+cp -r "$SOURCEDIR/simconf-templates" "$PKGROOT/usr/share/caraneer-config-nginx/templates"
+simconf toml-to-template "$SOURCEDIR/templates.toml" "$PKGROOT/DEBIAN/templates"
+
+sed "s/VERSION_REPLACEME/$version/" "$SOURCEDIR/DEBIAN/control" > "$PKGROOT/DEBIAN/control"
+
+echo "Building package..."
+mkdir -p "${POOL_DIR}/caraneer-config-nginx"
+dpkg-deb --build "$PKGROOT" "${POOL_DIR}/caraneer-config-nginx/caraneer-config-nginx_${version}_all.deb"
+echo "Done: ${POOL_DIR}/caraneer-config-nginx/caraneer-config-nginx_${version}_all.deb"
