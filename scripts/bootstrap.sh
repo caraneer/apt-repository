6b25e3d33af4df50dbda2c48992d9cea56650771
ihatecsv <drakeluce@gmail.com>
2025-05-07 19:00:13 +0000

    typos

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,18 +4,18 @@ set -euo pipefail
 SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
 
 sudo apt update
-sudo apt install -y --no-install-recommends sshfs
+sudo apt install -y --no-install-recommends SSH
 
 mkdir -p ~/.ssh
-echo "${SSHFS_PRIVATE_KEY}" > ~/.ssh/id_rsa
+echo "${SSH_KEY}" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-ssh-keyscan -H "${SSHFS_HOST}" >> ~/.ssh/known_hosts
+ssh-keyscan -H "${SSH_HOST}" >> ~/.ssh/known_hosts
 
-mkdir -p "${SSHFS_MOUNT_PATH}"
+mkdir -p "${SSH_MOUNT_PATH}"
 sshfs -o IdentityFile=~/.ssh/id_rsa,StrictHostKeyChecking=no \
-      "${SSHFS_USER}@${SSHFS_HOST}:${SSHFS_REMOTE_PATH}" \
-      "${SSHFS_MOUNT_PATH}"
+      "${SSH_USER}@${SSH_HOST}:${SSH_REMOTE_PATH}" \
+      "${SSH_MOUNT_PATH}"
 
 readarray -t LOCAL_DEBS < <(find "${SCRIPT_DIR}/../bootstrap_pkgs" \
                            -maxdepth 1 -type f -name '*.deb' | sort)
