4c3b083c18123dfb9d79ac82729a48f0f1fa1f2e
ihatecsv <drakeluce@gmail.com>
2025-05-07 19:03:03 +0000

    or true

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)
 
 sudo apt update
-sudo apt install -y --no-install-recommends sshfs
+sudo apt install -y --no-install-recommends sshfs || true
 
 mkdir -p ~/.ssh
 echo "${SSH_KEY}" > ~/.ssh/id_rsa
