c35ec707747ccc606ffe11513a2153ebd870fe2e
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2025-05-09 16:39:03 -0400

    ensure nginx simconf templates are executed after initial acmetool run

--- configurators/acmetool/mkdeb.sh
+++ configurators/acmetool/mkdeb.sh
@@ -5,7 +5,7 @@ SOURCEDIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P);
 WORKDIR="$(mktemp -d -t acmetool-deb.XXXXXXXXXX)"
 echo "WORKDIR=$WORKDIR";
 PKGROOT="$WORKDIR/pkg"
-version="0.1.0-1"
+version="0.2.0-1"
 
 echo "Preparing package directory..."
 mkdir -p "$PKGROOT";
