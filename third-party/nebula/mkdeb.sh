5f51aae49056efaa81374a84f4d59209bea1c702
ihatecsv <drakeluce@gmail.com>
2025-05-27 20:27:57 +0000

    reverse lighthouse static_host_map cond.

--- third-party/nebula/mkdeb.sh
+++ third-party/nebula/mkdeb.sh
@@ -11,7 +11,7 @@ release_json=$(gh release view --repo slackhq/nebula --json tagName,assets)
 tag=$(jq -r '.tagName' <<<"$release_json")
 version="${tag#v}"
 if [[ "${version}" = "1.9.5" ]]; then
-    version="${version}-7"
+    version="${version}-8"
 else
     version="${version}-1"
 fi;
