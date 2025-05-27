747aea420a1dc83a1efab61fc260cc22ee6909ad
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2025-05-27 16:48:47 -0400

    don't care if file already exists

--- third-party/nebula/mkdeb.sh
+++ third-party/nebula/mkdeb.sh
@@ -11,7 +11,7 @@ release_json=$(gh release view --repo slackhq/nebula --json tagName,assets)
 tag=$(jq -r '.tagName' <<<"$release_json")
 version="${tag#v}"
 if [[ "${version}" = "1.9.5" ]]; then
-    version="${version}-9"
+    version="${version}-10"
 else
     version="${version}-1"
 fi;
