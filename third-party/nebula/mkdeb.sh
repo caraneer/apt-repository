85af2f7fb98c39012a40d1a081bbe0952016fa14
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2025-05-28 17:56:29 -0400

    fix typo

--- third-party/nebula/mkdeb.sh
+++ third-party/nebula/mkdeb.sh
@@ -11,7 +11,7 @@ release_json=$(gh release view --repo slackhq/nebula --json tagName,assets)
 tag=$(jq -r '.tagName' <<<"$release_json")
 version="${tag#v}"
 if [[ "${version}" = "1.9.5" ]]; then
-    version="${version}-11"
+    version="${version}-12"
 else
     version="${version}-1"
 fi;
