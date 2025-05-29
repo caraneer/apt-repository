21b570095a50762239899cf71200024d144e2769
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2025-05-29 10:01:45 -0400

    remove domain filter as it's network interface specific now anyway

--- third-party/nebula/mkdeb.sh
+++ third-party/nebula/mkdeb.sh
@@ -11,7 +11,7 @@ release_json=$(gh release view --repo slackhq/nebula --json tagName,assets)
 tag=$(jq -r '.tagName' <<<"$release_json")
 version="${tag#v}"
 if [[ "${version}" = "1.9.5" ]]; then
-    version="${version}-13"
+    version="${version}-14"
 else
     version="${version}-1"
 fi;
