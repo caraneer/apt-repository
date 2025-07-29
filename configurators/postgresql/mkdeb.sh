87a2840053289e4941cec099ba4b0184cbceddfc
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2025-07-29 17:24:50 -0400

    handle more subscription edgecases

--- configurators/postgresql/mkdeb.sh
+++ configurators/postgresql/mkdeb.sh
@@ -5,7 +5,7 @@ SOURCEDIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P);
 WORKDIR="$(mktemp -d -t postgresql-deb.XXXXXXXXXX)"
 echo "WORKDIR=$WORKDIR";
 PKGROOT="$WORKDIR/pkg"
-version="0.1.6-1"
+version="0.1.7-1"
 
 echo "Preparing package directory..."
 mkdir -p "$PKGROOT";
@@ -15,6 +15,8 @@ cp -r "$SOURCEDIR/DEBIAN" "$PKGROOT/DEBIAN"
 mkdir -p "$PKGROOT/usr/share/caraneer-config-postgresql";
 cp -r "$SOURCEDIR/simconf-templates" "$PKGROOT/usr/share/caraneer-config-postgresql/templates"
 cp -r "$SOURCEDIR/bin" "$PKGROOT/usr/bin"
+mkdir -p "$PKGROOT/usr/lib/systemd";
+cp -r "$SOURCEDIR/systemd" "$PKGROOT/usr/lib/systemd/system"
 simconf toml-to-template "$SOURCEDIR/templates.toml" "$PKGROOT/DEBIAN/templates"
 
 sed "s/VERSION_REPLACEME/$version/" "$SOURCEDIR/DEBIAN/control" > "$PKGROOT/DEBIAN/control"
