5a4b9314f41e5a9b6aa9c1a72f2d2d718c145466
ihatecsv <drakeluce@gmail.com>
2025-08-20 19:52:28 +0000

    Vector configurator no longer manage docker

--- configurators/vector/mkdeb.sh
+++ configurators/vector/mkdeb.sh
@@ -5,7 +5,7 @@ SOURCEDIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P);
 WORKDIR="$(mktemp -d -t vector-deb.XXXXXXXXXX)"
 echo "WORKDIR=$WORKDIR";
 PKGROOT="$WORKDIR/pkg"
-version="0.1.1-1"
+version="0.2.1-1"
 
 echo "Preparing package directory..."
 mkdir -p "$PKGROOT/DEBIAN";
